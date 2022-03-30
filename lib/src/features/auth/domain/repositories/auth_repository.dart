a4c22cabfcc20580437877b99c4138e177a04570
Jonathas Tassi <jonathastassi@hotmail.com>
2022-03-30 14:12:56 -0300

    Created interface of authRepository

---
+++ lib/src/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_blog_app/src/core/errors/failures.dart';
+import 'package:flutter_blog_app/src/features/auth/domain/entities/login_entity.dart';
+import 'package:flutter_blog_app/src/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, UserEntity>> login(LoginEntity loginEntity);
+}
