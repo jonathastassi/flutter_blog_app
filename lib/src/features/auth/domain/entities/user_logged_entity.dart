8771b2000aabf19e72a5bd09361e71350ff3fb2b
Jonathas Tassi <jonathastassi@hotmail.com>
2022-03-30 14:10:47 -0300

    Improved UserLoggedEntity constructor

--- lib/src/features/auth/domain/entities/user_logged_entity.dart
+++ lib/src/features/auth/domain/entities/user_logged_entity.dart
@@ -2,7 +2,10 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_blog_app/src/features/auth/domain/entities/user_entity.dart';
 
 class UserLoggedEntity extends Equatable {
-  UserLoggedEntity(UserEntity user, this.token) : user = user.withoutPassword();
+  const UserLoggedEntity({
+    required this.user,
+    required this.token,
+  });
 
   final UserEntity user;
   final String token;
