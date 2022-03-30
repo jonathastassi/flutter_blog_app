b03284ef0502088432ee3670f36e509b6014c579
Jonathas Tassi <jonathastassi@hotmail.com>
2022-03-30 20:08:57 -0300

    Added register method on AuthRepository

--- lib/src/features/auth/domain/repositories/auth_repository.dart
+++ lib/src/features/auth/domain/repositories/auth_repository.dart
@@ -1,8 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_blog_app/src/core/errors/failures.dart';
 import 'package:flutter_blog_app/src/features/auth/domain/entities/login_entity.dart';
+import 'package:flutter_blog_app/src/features/auth/domain/entities/register_entity.dart';
 import 'package:flutter_blog_app/src/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepository {
   Future<Either<Failure, UserEntity>> login(LoginEntity loginEntity);
+  Future<Either<Failure, UserEntity>> register(RegisterEntity registerEntity);
 }
