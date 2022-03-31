f9e47db49fce9578292f743c077aa0cdbb8c189f
Jonathas Tassi <jonathastassi@hotmail.com>
2022-03-31 13:20:40 -0300

    Implemented AuthRepository and UserLogged and user models

---
+++ lib/src/core/constants/contants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static String keyUserLoggedStored = 'user_logged_stored';
+}
---
+++ lib/src/features/auth/infra/models/user_logged_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_blog_app/src/features/auth/domain/entities/user_entity.dart';
+import 'package:flutter_blog_app/src/features/auth/domain/entities/user_logged_entity.dart';
+import 'package:flutter_blog_app/src/features/auth/infra/models/user_model.dart';
+
+class UserLoggedModel extends UserLoggedEntity {
+  const UserLoggedModel({
+    required UserEntity user,
+    required String authorization,
+  }) : super(
+          user: user,
+          authorization: authorization,
+        );
+
+  factory UserLoggedModel.fromJson(Map<String, dynamic> json) {
+    return UserLoggedModel(
+      user: UserModel.fromJson(json['user']),
+      authorization: json['authorization'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['user'] = UserModel(
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    ).toJson();
+    data['authorization'] = authorization;
+    return data;
+  }
+}
