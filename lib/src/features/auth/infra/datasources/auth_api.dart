866b90e2263cfc311dffae1d6b74901046f3b16b
Jonathas Tassi <jonathastassi@hotmail.com>
2022-03-31 20:08:00 -0300

    Implemented AuthApi

--- lib/src/features/auth/infra/datasources/auth_api.dart
+++ lib/src/features/auth/infra/datasources/auth_api.dart
@@ -1,12 +1,12 @@
-import 'package:flutter_blog_app/src/features/auth/domain/entities/user_logged_entity.dart';
+import 'package:flutter_blog_app/src/features/auth/infra/models/user_logged_model.dart';
 
 abstract class AuthApi {
-  Future<UserLoggedEntity?> login({
+  Future<UserLoggedModel?> login({
     required String email,
     required String password,
   });
 
-  Future<UserLoggedEntity> register({
+  Future<UserLoggedModel> register({
     required String name,
     required String email,
     required String password,
---
+++ lib/src/features/auth/infra/datasources/auth_api_impl.dart
@@ -0,0 +1,44 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_blog_app/src/features/auth/infra/datasources/auth_api.dart';
+import 'package:flutter_blog_app/src/features/auth/infra/models/user_logged_model.dart';
+import 'package:flutter_blog_app/src/features/auth/infra/models/user_model.dart';
+
+class AuthApiImpl implements AuthApi {
+  AuthApiImpl({Faker? faker}) : _faker = faker ?? Faker();
+
+  final Faker _faker;
+
+  @override
+  Future<UserLoggedModel?> login({
+    required String email,
+    required String password,
+  }) async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (email == 'emailvalido@gmail.com' && password == '12345678') {
+      return UserLoggedModel(
+        user: UserModel(
+          id: _faker.guid.guid(),
+          email: _faker.internet.email(),
+          name: _faker.person.name(),
+        ),
+        authorization: _faker.jwt.valid(),
+      );
+    }
+    return null;
+  }
+
+  @override
+  Future<UserLoggedModel> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    return UserLoggedModel(
+      user: UserModel(id: _faker.guid.guid(), email: email, name: name),
+      authorization: _faker.jwt.valid(),
+    );
+  }
+}
