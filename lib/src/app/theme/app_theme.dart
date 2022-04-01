6d03c00155542454a3574aab38e349714810dd09
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-01 17:45:00 -0300

    Created theme

---
+++ lib/src/app/theme/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData(
+  // primaryColorDark: const Color(0xFF0097A7),
+  // primaryColorLight: const Color(0xFFB2EBF2),
+  // colorScheme: const ColorScheme.light(secondary: Color(0xFF009688)),
+  scaffoldBackgroundColor: const Color(0xFFFDFAF6),
+  progressIndicatorTheme: const ProgressIndicatorThemeData(
+    color: Color(0xFFFDFAF6),
+  ),
+  // inputDecorationTheme: InputDecorationTheme(
+  // border: OutlineInputBorder(
+  // borderRadius: BorderRadius.circular(8),
+  // ),
+  // ),
+  textTheme: const TextTheme(
+    headline1: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+    headline2: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
+    headline3: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+    headline4: TextStyle(
+      fontSize: 28.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFFDFAF6),
+    ),
+    headline5: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+    // bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+  ),
+  colorScheme: const ColorScheme.light().copyWith(
+    primary: const Color(0xFF064420),
+    secondary: const Color(0xFFE4EFE7),
+    onPrimary: const Color(0xFFFDFAF6),
+    onSecondary: const Color(0xFFFAF1E6),
+  ),
+);
