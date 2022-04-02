5b44e0f223eeedaef30589ab727a93f2f832a701
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-02 19:38:56 -0300

    Logout temporary in home

--- lib/src/features/home/presenter/view/home_page.dart
+++ lib/src/features/home/presenter/view/home_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/src/app/bloc/app_cubit.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final AppCubit cubit = Modular.get();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: cubit.logout,
+            icon: const Icon(
+              Icons.logout,
+            ),
+          )
+        ],
       ),
       body: Container(),
     );
