654a32cdbd4ebd25e0d02f31ffa221279a197ec9
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-02 16:07:05 -0300

    HomePage cleaned

--- lib/src/features/home/presenter/view/home_page.dart
+++ lib/src/features/home/presenter/view/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog_app/src/app/bloc/app_cubit.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,11 +7,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
       ),
-      body: Text(context.read<AppCubit>().userLogged != null
-          ? context.read<AppCubit>().userLogged!.user.name
-          : "No user"),
+      body: Container(),
     );
   }
 }
