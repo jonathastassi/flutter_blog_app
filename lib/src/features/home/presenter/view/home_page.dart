156c8c917a76c5a3bc602a79ced2ac5724b45eca
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-03 20:44:07 -0300

    Home initial layout

--- lib/src/features/home/presenter/view/home_page.dart
+++ lib/src/features/home/presenter/view/home_page.dart
@@ -7,20 +7,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final AppCubit cubit = Modular.get();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-        actions: [
-          IconButton(
-            onPressed: cubit.logout,
-            icon: const Icon(
-              Icons.logout,
-            ),
-          )
-        ],
+    final tabs = [
+      const Tab(
+        icon: Icon(Icons.home),
+        text: 'Feed',
       ),
-      body: Container(),
+      const Tab(
+        icon: Icon(Icons.newspaper),
+        text: 'Novidades',
+      )
+    ];
+
+    final pages = [
+      Center(
+        child: Text('Feed'),
+      ),
+      Center(
+        child: Text('Novidades'),
+      ),
+    ];
+
+    final AppCubit cubit = Modular.get();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
+          actions: [
+            IconButton(
+              onPressed: cubit.logout,
+              icon: const Icon(
+                Icons.logout,
+              ),
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: pages,
+        ),
+        bottomNavigationBar: Material(
+          elevation: 0,
+          color: Theme.of(context).colorScheme.primary,
+          child: TabBar(
+            indicatorColor: Theme.of(context).colorScheme.onPrimary,
+            tabs: tabs,
+          ),
+        ),
+      ),
     );
   }
 }
