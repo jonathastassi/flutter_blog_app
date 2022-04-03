44dbed9034355ecd54bcb868831b5578622c4c9e
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-03 20:24:28 -0300

    Added confirmation password validator

--- lib/src/core/validators/password_validator.dart
+++ lib/src/core/validators/password_validator.dart
@@ -3,10 +3,22 @@ mixin PasswordValidator {
     if (value == null || value.isEmpty) {
       return 'Digite uma senha válida';
     }
-    if (value.length <= 6) {
+    if (value.length < 6) {
       return 'A senha deve ter no mínimo 6 caracteres';
     }
 
     return null;
+  }
+
+  String? validateConfirmPassword(String? value, String? password) {
+    if (value == null || value.isEmpty) {
+      return 'Digite uma confirmação de senha válida';
+    }
+
+    if (value == password) {
+      return null;
+    }
+
+    return 'Confirmação de senha não confere';
   }
 }
