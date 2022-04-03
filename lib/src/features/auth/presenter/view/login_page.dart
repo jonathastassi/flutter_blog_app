4949ed072bda44dd8b371767e9158a6ee5cdee76
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-03 16:40:16 -0300

    Login page flow finalized

--- lib/src/features/auth/presenter/view/login_page.dart
+++ lib/src/features/auth/presenter/view/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_blog_app/src/core/validators/email_validator.dart';
+import 'package:flutter_blog_app/src/core/validators/password_validator.dart';
 import 'package:flutter_blog_app/src/features/auth/presenter/bloc/login_cubit.dart';
 import 'package:flutter_blog_app/src/features/auth/presenter/bloc/login_state.dart';
 import 'package:flutter_blog_app/src/shared/widgets/outlined_button_custom.dart';
@@ -12,14 +14,17 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends ModularState<LoginPage, LoginCubit> {
+class _LoginPageState extends ModularState<LoginPage, LoginCubit>
+    with EmailValidator, PasswordValidator {
   late FocusNode _emailFocus;
+  late GlobalKey<FormState> _formKey;
 
   @override
   void initState() {
     super.initState();
 
     _emailFocus = FocusNode();
+    _formKey = _formKey = GlobalKey<FormState>();
   }
 
   @override
@@ -34,93 +39,108 @@ class _LoginPageState extends ModularState<LoginPage, LoginCubit> {
       backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
         title: const Text('Realize o login'),
-        elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              height: 90,
-            ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: 'Together',
-                style: Theme.of(context).textTheme.headline4,
-                children: <TextSpan>[
-                  TextSpan(
-                    text: ' Blog',
-                    style: Theme.of(context).textTheme.headline4!.copyWith(
-                          fontWeight: FontWeight.normal,
-                        ),
-                  )
-                ],
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(
+                height: 90,
               ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Card(
-              margin: const EdgeInsets.all(16),
-              color: Theme.of(context).colorScheme.secondary,
-              child: BlocConsumer<LoginCubit, LoginState>(
-                listener: (context, state) {
-                  if (state.error != '') {
-                    _emailFocus.requestFocus();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(state.error),
+              Hero(
+                tag: 'splash_title',
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: 'Together',
+                    style: Theme.of(context).textTheme.headline4,
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: ' Blog',
+                        style: Theme.of(context).textTheme.headline4!.copyWith(
+                              fontWeight: FontWeight.normal,
+                            ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Card(
+                margin: const EdgeInsets.all(16),
+                color: Theme.of(context).colorScheme.secondary,
+                child: BlocConsumer<LoginCubit, LoginState>(
+                  listener: (context, state) {
+                    if (state.error != '') {
+                      _emailFocus.requestFocus();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(state.error),
+                        ),
+                      );
+                    }
+                  },
+                  bloc: cubit,
+                  builder: (context, state) {
+                    return Form(
+                      key: _formKey,
+                      autovalidateMode: AutovalidateMode.always,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              enabled: !state.loading,
+                              focusNode: _emailFocus,
+                              keyboardType: TextInputType.emailAddress,
+                              validator: validateEmail,
+                              decoration: InputDecoration(
+                                labelText: 'E-mail',
+                                errorText:
+                                    state.error == '' ? null : state.error,
+                              ),
+                              onChanged: bloc.setEmail,
+                            ),
+                            TextFormField(
+                              enabled: !state.loading,
+                              validator: validatePassword,
+                              decoration: const InputDecoration(
+                                labelText: 'Senha',
+                              ),
+                              obscureText: true,
+                              onChanged: bloc.setPassword,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            OutlinedButtonCustom(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  bloc.login();
+                                }
+                              },
+                              child: const Text('Acessar'),
+                              loading: bloc.state.loading,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            TextButton(
+                              onPressed: bloc.register,
+                              child: const Text('Criar uma conta'),
+                            ),
+                          ],
+                        ),
                       ),
                     );
-                  }
-                },
-                bloc: cubit,
-                builder: (context, state) {
-                  return Form(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            focusNode: _emailFocus,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                              labelText: 'E-mail',
-                              errorText: state.error == '' ? null : state.error,
-                            ),
-                            onChanged: bloc.setEmail,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: 'Senha',
-                            ),
-                            obscureText: true,
-                            onChanged: bloc.setPassword,
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          OutlinedButtonCustom(
-                            onPressed: bloc.login,
-                            child: const Text('Acessar'),
-                            loading: bloc.state.loading,
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          TextButton(
-                            onPressed: bloc.register,
-                            child: const Text('Criar uma conta'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
