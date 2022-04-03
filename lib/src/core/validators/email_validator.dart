718c3e1a6ca95edb4557cc935742fca5e3aa6a00
Jonathas Tassi <jonathastassi@hotmail.com>
2022-04-03 16:39:53 -0300

    Email and password validators

---
+++ lib/src/core/validators/email_validator.dart
@@ -0,0 +1,13 @@
+mixin EmailValidator {
+  String? validateEmail(String? value) {
+    String pattern =
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?)*$";
+    RegExp regex = RegExp(pattern);
+    if (value == null || value.isEmpty || !regex.hasMatch(value)) {
+      return 'Digite um e-mail válido';
+    }
+    return null;
+  }
+}
